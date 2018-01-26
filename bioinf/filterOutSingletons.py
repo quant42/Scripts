04af25bac5a6d14f0f7e4c0714018a056d3f2216
Yann <ga29qal@mytum.de>
2018-01-26 07:37:24 +0100

    filter out singletons

---
+++ bioinf/filterOutSingletons.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from __future__ import division, print_function
+from Bio import SeqIO
+import sys
+
+getName = lambda x : x if "_" not in x else x[:x.rfind("_")]
+data = {}
+
+with open(sys.argv[1], "r") as inF:
+    for record in SeqIO.parse(inF, "fasta"):
+        fullName = record.name
+        seq = str(record.seq)
+        l = data.get(seq, [])
+        l.append(fullName)
+        data[seq] = l
+
+#print(data)
+
+# now filter out all singletons
+with open(sys.argv[2], "w") as outF:
+    for seq in data:
+        names = data[seq]
+        indNames = set()
+        for name in names:
+            indNames.add(getName(name))
+        if indNames >= 2:
+            for name in names:
+                outF.write(">%s\n%s\n" % (name, seq))
